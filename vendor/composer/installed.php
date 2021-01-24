5f5ebf39d5026b1e827b15a8ae6e384db75a04c3
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2021-01-24 19:15:21 +0700

    updated to v3.9.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2bbe3180591e070d57296585b4e5c04b420074f9',
+    'name' => 'ofi/ofi_php_framework',
+  ),
+  'versions' => 
+  array (
+    'filp/whoops' => 
+    array (
+      'pretty_version' => '2.9.1',
+      'version' => '2.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '307fb34a5ab697461ec4c9db865b20ff2fd40771',
+    ),
+    'ofi/ofi_php_framework' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2bbe3180591e070d57296585b4e5c04b420074f9',
+    ),
+    'plasticbrain/php-flash-messages' => 
+    array (
+      'pretty_version' => 'v1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b565762619342057ce561de20d0f2f6e14f9aa77',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+  ),
+);
